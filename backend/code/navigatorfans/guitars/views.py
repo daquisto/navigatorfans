72ef06ad7e574ac7ac1e1f8c521a2bcbbdcfd804
Ramon Goose <ramongoosemusic@gmail.com>
2022-11-20 21:58:03 +0000

    feature - add detail view

--- backend/code/navigatorfans/guitars/views.py
+++ backend/code/navigatorfans/guitars/views.py
@@ -1,10 +1,21 @@
 from django.views import generic
 
+from .forms  import GuitarForm
 from .models import Guitar
 
 
 class GuitarListView(generic.ListView):
     model = Guitar
+
+
+class GuitarDetailView(generic.DetailView):
+    model = Guitar
+
+
+class GuitarEditView(generic.edit.FormView):
+    template_name = 'guitars/guitar_edit.html'
+    form_class = GuitarForm
+
 
 
 
